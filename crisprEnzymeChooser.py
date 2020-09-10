6ba2e828bf952979a70628d210681096ec8422a2
Samuel Fay <smafay6@gmail.com>
2020-09-10 15:50:24 -0600

    Got functionality working. The most efficient sequence comparison is still not working quite right.

--- crisprEnzymeChooser.py
+++ crisprEnzymeChooser.py
@@ -31,15 +31,19 @@ class EnzymeChooser:
         self.inputSeq = inputSeq
         self.enzymeFile = enzymeFile
         self.windowLength = windowLength
-        # list of lists with the possible codons for each codon
+        # list for current permutations
         self.permutationsList = []
         # output dict with enzymes as the keys, then the value is
-        # a tuple: (binding sequence, shortened binding sequence, least-
-        # different input sequence permutation (defaults to empty string),
-        # enzyme starting index) shortened means the leading and trailing
+        # a list: [binding sequence, shortened binding sequence, least-
+        # different input sequence window permutation (defaults to empty string),
+        # enzyme starting index, overall best sequence with length of the input]
+        # shortened means the leading and trailing
         # N's have been stripped from the sequence.
         # if the starting index is -1, then the enzyme hasn't been found
         self.enzymes = {}
+        # variables for the start and end of the window
+        self.windowStart = 0
+        self.windowEnd = 0
 
     # make a dictionary of enzymes and their sequences
     def __makeEnzymesDict(self):
@@ -47,23 +51,30 @@ class EnzymeChooser:
         for line in enzymeLineList:
             splitLine = line.split()
             # key: enzyme, value: sequence
-            self.enzymes[splitLine[0]] = (splitLine[1], \
-                splitLine[1].split("N"), "", -1)
+            self.enzymes[splitLine[0]] = [splitLine[1], \
+                splitLine[1].strip("N"), "", -1, ""]
 
     # make the list of all codons that could be interchanged
     # at each codon place while keeping the overall codons the same
-    def __makePermuationsList(self):
-        codons = Seq.translate(Seq(input, generic_dna))
+    def __makePermuationsList(self, slice):
+        # clear list
+        self.permutationsList = []
+        codons = Seq.translate(Seq(slice, generic_dna))
+        #print(codons)
         for c in codons:
             self.permutationsList.append( \
                 [k for k,v in self.fwdTable.items() if v == c])
+        #print(self.permutationsList)
 
     # compares two sequences using the compare dictionary
-    def __compareSequences(self, s1, s2):
-        if len(s1) != len(s2):
+    # enzyme seq can have ambiguities, dna seq can't
+    def __compareSequences(self, dnaSeq, enzymeSeq):
+        if len(dnaSeq) != len(enzymeSeq):
             return False
-        for i in range(len(s1)):
-            if s1[i] == s2[i] or s2[i] in nucleotideEqualities[s1[i]]:
+        for i in range(len(dnaSeq)):
+            if dnaSeq[i] == enzymeSeq[i] or (enzymeSeq[i] in \
+                EnzymeChooser.nucleotideEqualities and dnaSeq[i] in \
+                EnzymeChooser.nucleotideEqualities[enzymeSeq[i]]):
                 continue
             else:
                 return False
@@ -79,75 +90,76 @@ class EnzymeChooser:
                 differences += 1
         return differences
 
+
     # searching for enzymes using a scrolling window of windowLength
     # returns a list of enzymes and the location that they start at
     # in the sequence
     def __searchForEnzymes(self, sequence):
-        windowStart = 0
-        windowEnd = self.windowLength
         sequenceDiff = self.__calculateSequenceDifferences(sequence)
-        if (windowEnd > len(sequence)):
-            windowEnd = len(sequence)
-        # while the moving window doesn't hit the end
-        while (windowEnd <= len(sequence)):
-            # get the current base pairs in the window
-            slice = sequence[windowStart:windowEnd]
-            # check for every enzyme starting at every character in slice
-            for i in range(len(slice)):
-                lengthLeft = self.windowLength - i
-                # iterate through every enzyme
-                for key in self.enzymes:
-                    currentEnzyme = self.enzymes[key]
-                    # if we have already found this enzyme starting on this
-                    # character in the sequence then we don't need to find it
-                    # again
-                    if currentEnzyme[3] == i + windowStart:
-                        continue
-                    enzymeLength = len(currentEnzyme[1])
-                    # if the enzyme won't fit, we move on to the next
-                    if enzymeLength > lengthLeft:
-                        continue
-                    if self.__compareSequences(slice[i:i+enzymeLength]):
-                        # if we haven't found one yet or if the current sequence
-                        # is closer to the original, then we replace it
-                        if currentEnzyme[2] == "" or \
-                            self.__calculateSequenceDifferences( \
-                            currentEnzyme[2]) > sequenceDiff:
-                            currentEnzyme[2] = sequence
-                            currentEnzyme[3] = i + windowStart
-            windowStart += 1
-            windowEnd += 1
+        # check for every enzyme starting at every character in slice
+        for i in range(len(sequence)):
+            lengthLeft = self.windowLength - i
+            # iterate through every enzyme
+            for key in self.enzymes:
+                currentEnzyme = self.enzymes[key]
+                # if we have already found this enzyme starting on this
+                # character in the sequence then we don't need to find it
+                # again
+                if currentEnzyme[3] == i + self.windowStart:
+                    continue
+                enzymeLength = len(currentEnzyme[1])
+                # if the enzyme won't fit, we move on to the next
+                if enzymeLength > lengthLeft:
+                    continue
+                if self.__compareSequences(sequence[i:i+enzymeLength], currentEnzyme[1]):
+                    # if we haven't found one yet or if the current sequence
+                    # is closer to the original, then we replace it
+                    if currentEnzyme[2] == "" or \
+                        self.__calculateSequenceDifferences( \
+                        currentEnzyme[2]) > sequenceDiff:
+                        # we found one!
+                        currentEnzyme[2] = sequence
+                        currentEnzyme[3] = i + self.windowStart
+                        currentEnzyme[4] = self.inputSeq[0:self.windowStart] + \
+                            sequence + self.inputSeq[self.windowEnd:len(self.inputSeq)]
 
 
     # recursive function that goes through possible permutations
+    # of each scrolling window
     # and stores the previous permutations in previousIndicies
-    def __permutateAndSearch(self, previousIndicies, currentIndex):
+    def __permutate(self, previousIndicies, currentIndex):
         # We have completed a permuation
         if (currentIndex == len(self.permutationsList)):
             newSequence = ""
             # make the new complete permutation based on the previous indicies
             for i in range(len(previousIndicies)):
                 newSequence += self.permutationsList[i][previousIndicies[i]]
-            enzymesListAndLocations = searchForEnzymes(newSequence)
-            for enzymeAndLocation in enzymesList:
-                # check if we have already found this enzyme so far
-                if enzymeAndLocation[0] in self.validEnzymes:
-                    self.validEnzymes[enzymeAndLocation[0]].append( \
-                        (newSequence, enzymeAndLocation[1])
-                # if we haven't make a new entry
-                else:
-                    self.validEnzymes[enzymeAndLocation[0]] = \
-                        [(newSequence, enzymeAndLocation[1])]
+            #print("Current sequence:" + newSequence)
+            #print("Current codons: " + Seq.translate(Seq(newSequence, generic_dna)))
+            self.__searchForEnzymes(newSequence)
         # Keep going with different options for currentIndex
         else:
-            for i in range(len(list[currentIndex])):
-                self.permutate(list, previousIndicies + [i], currentIndex + 1)
+            for i in range(len(self.permutationsList[currentIndex])):
+                self.__permutate(previousIndicies + [i], currentIndex + 1)
 
     # only public function, runs all the other functions to
     # get the final product
     def getEnzymes(self):
         self.__makeEnzymesDict()
-        self.__makePermuationsList()
+        self.windowStart = 0
+        self.windowEnd = self.windowLength
+        # while the moving window doesn't hit the end
+        while (self.windowEnd <= len(self.inputSeq)):
+            print("SEARCHING WINDOW FROM " + str(self.windowStart) + " TO " \
+                + str(self.windowEnd))
+            # get the current base pairs in the window
+            slice = self.inputSeq[self.windowStart:self.windowEnd]
+            #print(slice)
+            self.__makePermuationsList(slice)
+            self.__permutate([], 0)
+            self.windowStart += 3
+            self.windowEnd += 3
+        return self.enzymes
 
 
 
@@ -155,5 +167,11 @@ input = "GATCGATGGGCCTATATAGGATCGAAAATC"
 enzymesFile = "enzymes.txt"
 windowLength = 15
 chooser = EnzymeChooser(input, enzymesFile, windowLength)
-validEnzymes = chooser.getEnzymes()
-print(validEnzymes)
+enzymes = chooser.getEnzymes()
+found = 0
+for key in enzymes:
+    if enzymes[key][3] != -1 and len(enzymes[key][1]) >= 6:
+        found += 1
+        print(key)
+        print(enzymes[key])
+print("Found " + str(found) + " enzymes.")
